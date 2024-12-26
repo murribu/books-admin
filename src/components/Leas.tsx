a95ca5d2dcb13c300e4ca7edcb3cd45af36b7934
Cory Martin <murribu@gmail.com>
2024-12-26 16:59:52 -0600

    feat: Refactor Leas component and add sorting
    Refactored Leas component to use React Bootstrap Table and Button components. Added sorting by name or score using header buttons.  Improved UI and UX.

--- src/components/Leas.tsx
+++ src/components/Leas.tsx
@@ -1,21 +1,37 @@
+import { Button, Table } from "react-bootstrap";
 import { useBookContext } from "../contexts/bookContext";
+import { useState } from "react";
 
 const Leas = () => {
   const { leas } = useBookContext();
+  const [sortBy, setSortBy] = useState<string>("name");
+
+  const sortLeas = (a: any, b: any) => {
+    switch (sortBy) {
+      case "score":
+        return b.score - a.score;
+      default:
+        return a[sortBy].localeCompare(b[sortBy]);
+    }
+  };
   return (
     <div>
       <h1>LEAs</h1>
-      <table>
+      <Table>
         <thead>
           <tr>
             <th>County</th>
             <th>ID</th>
-            <th>Name</th>
-            <th>Score</th>
+            <th>
+              <Button onClick={() => setSortBy("name")}>Name</Button>
+            </th>
+            <th>
+              <Button onClick={() => setSortBy("score")}>Score</Button>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {leas.map((lea) => (
+          {leas.sort(sortLeas).map((lea) => (
             <tr key={lea.id}>
               <td>{lea.county}</td>
               <td>{lea.id}</td>
@@ -24,7 +40,7 @@ const Leas = () => {
             </tr>
           ))}
         </tbody>
-      </table>
+      </Table>
     </div>
   );
 };
